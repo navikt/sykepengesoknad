369b4694fc6c3b0a8862d6b7b574548be48fc3fe
Helge Fredheim <helge.fredheim@bekk.no>
2019-05-23 15:21:39 +0200

    Legger tilbake lside for å opprette sykepengesøknad-utland

--- js/routers/AppRouter.js
+++ js/routers/AppRouter.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import { Route, Router } from 'react-router';
 import SoknaderSide from '../sykepengesoknad-gammel-plattform/sider/SoknaderSide';
 import SykepengesoknadContainer from '../sykepengesoknad/sider/SoknadSide';
+import FoerDuBegynnerUtlandContainer from '../sykepengesoknad/soknad-utland/for-du-begynner/FoerDuBegynnerUtlandContainer';
 
 const AppRouter = ({ history }) => {
     return (<Router history={history}>
         <Route path={`${process.env.REACT_APP_CONTEXT_ROOT}/soknader/:sykepengesoknadId`} component={SykepengesoknadContainer} />
         <Route path={`${process.env.REACT_APP_CONTEXT_ROOT}/soknader/:sykepengesoknadId/:steg`} component={SykepengesoknadContainer} />
+        <Route path={`${process.env.REACT_APP_CONTEXT_ROOT}/sykepengesoknad-utland`} component={FoerDuBegynnerUtlandContainer} />
         <Route path="*" component={SoknaderSide} />
     </Router>);
 };
