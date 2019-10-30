d07e63e250cc757a5f3670e320829a16080b8508
Kent Daleng <kent.stefan.daleng@nav.no>
2019-10-30 12:00:52 +0100

    Fjerner amplitude-kode enn så lenge

--- js/routers/AppRouter.js
+++ js/routers/AppRouter.js
@@ -1,34 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Router } from 'react-router';
-import amplitude from 'amplitude-js';
-import { AmplitudeProvider } from '@amplitude/react-amplitude/src';
 import SoknaderSide from '../sykepengesoknad-gammel-plattform/sider/SoknaderSide';
 import SykepengesoknadContainer from '../sykepengesoknad/sider/SoknadSide';
 import FoerDuBegynnerUtlandContainer from '../sykepengesoknad/soknad-utland/for-du-begynner/FoerDuBegynnerUtlandContainer';
 
 
 const AppRouter = ({ history }) => {
-    return (<AmplitudeProvider amplitudeInstance={amplitude.getInstance().init(
-        `${process.env.AMPLITUDE_APIKEY}`, null, {
-            apiEndpoint: 'amplitude.nav.no/collect',
-            saveEvents: true,
-            includeUtm: true,
-            includeReferrer: true,
-            trackingOptions: {
-                city: false,
-                ip_address: false,
-            },
-        },
-    )}>
-        console.log(process.env);
-        <Router history={history}>
-            <Route path={`${process.env.REACT_APP_CONTEXT_ROOT}/soknader/:sykepengesoknadId`} component={SykepengesoknadContainer} />
-            <Route path={`${process.env.REACT_APP_CONTEXT_ROOT}/soknader/:sykepengesoknadId/:steg`} component={SykepengesoknadContainer} />
-            <Route path={`${process.env.REACT_APP_CONTEXT_ROOT}/sykepengesoknad-utland`} component={FoerDuBegynnerUtlandContainer} />
-            <Route path="*" component={SoknaderSide} />
-        </Router>
-    </AmplitudeProvider>);
+    console.log(process.env);
+    return (<Router history={history}>
+        <Route path={`${process.env.REACT_APP_CONTEXT_ROOT}/soknader/:sykepengesoknadId`} component={SykepengesoknadContainer} />
+        <Route path={`${process.env.REACT_APP_CONTEXT_ROOT}/soknader/:sykepengesoknadId/:steg`} component={SykepengesoknadContainer} />
+        <Route path={`${process.env.REACT_APP_CONTEXT_ROOT}/sykepengesoknad-utland`} component={FoerDuBegynnerUtlandContainer} />
+        <Route path="*" component={SoknaderSide} />
+    </Router>);
 };
 
 AppRouter.propTypes = {
