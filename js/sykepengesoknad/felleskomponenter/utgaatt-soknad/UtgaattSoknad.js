55d0c88835fe185ac9432724235169943f91566c
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2020-01-13 10:22:47 +0100

    Håndter utgått søknad

---
+++ js/sykepengesoknad/felleskomponenter/utgaatt-soknad/UtgaattSoknad.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div className="panel">
+            <div className="hode">
+                <h1 className="hode__tittel">Søknaden er utgått</h1>
+                <p className="hode__melding">Søknaden er utgått fordi den ikke er sendt til NAV innen fristen.</p>
+            </div>
+        </div>
+    );
+};
+
