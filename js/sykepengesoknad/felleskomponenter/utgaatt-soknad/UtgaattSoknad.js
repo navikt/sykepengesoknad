1af91456dd00ae53932d5ec5cad870d37c4666c4
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2020-01-13 14:35:27 +0100

    Sett endelig tekst

--- js/sykepengesoknad/felleskomponenter/utgaatt-soknad/UtgaattSoknad.js
+++ js/sykepengesoknad/felleskomponenter/utgaatt-soknad/UtgaattSoknad.js
@@ -5,7 +5,7 @@ export default () => {
         <div className="panel">
             <div className="hode">
                 <h1 className="hode__tittel">Søknaden er utgått</h1>
-                <p className="hode__melding">Søknaden er utgått fordi den ikke er sendt til NAV innen fristen.</p>
+                <p className="hode__melding">Du får ikke åpnet denne søknaden fordi du ikke har sendt den innen fristen.</p>
             </div>
         </div>
     );
